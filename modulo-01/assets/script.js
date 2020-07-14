bad207e1c7ef3926b6c84a5695ef6a5958c1fa55
Felipe Bueno <felipebueno181@gmail.com>
2020-07-14 14:35:04 -0300

    Ajuste no evento DOM para input

--- modulo-01/assets/script.js
+++ modulo-01/assets/script.js
@@ -1,18 +1,22 @@
+
 const red = document.querySelector("#rg-red");
 const green = document.querySelector("#rg-green");
 const blue = document.querySelector("#rg-blue");
 const colorPicked = document.querySelector("#color-picked");
 const colorHex = document.querySelector("#color-hex > p");
 
+pickColor();
+
+red.addEventListener('input', pickColor);
+green.addEventListener('input', pickColor);
+blue.addEventListener('input', pickColor);
+
 function pickColor() {
-  // alert(`rgb(${red.value}, ${green.value}, ${blue.value})`);
   red.nextElementSibling.value = red.value;
   green.nextElementSibling.value = green.value;
   blue.nextElementSibling.value = blue.value;
 
   var color = `#${hex(parseInt(red.value))}${hex(parseInt(green.value))}${hex(parseInt(blue.value))}`;
-
-  console.log(color);
 
   colorPicked.setAttribute("style", `background: ${color}`);
   colorHex.innerHTML = color;
