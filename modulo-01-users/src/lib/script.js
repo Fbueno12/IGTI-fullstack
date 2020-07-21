cea31fa2ba8010b2a2aac2e4dd6ba1a332174315
Felipe Bueno <felipebueno181@gmail.com>
2020-07-21 11:37:35 -0300

    segundo desafio do módulo 01 - ajuste acentos

--- modulo-01-users/src/lib/script.js
+++ modulo-01-users/src/lib/script.js
@@ -43,8 +43,8 @@ function findUsers(text, users) {
   body.innerHTML = "";
 
   const filtered = users.filter(value => {
-    const first = value.name.first.toLowerCase();
-    const last  = value.name.last.toLowerCase();
+    const first = removeSpecial(value.name.first.toLowerCase());
+    const last  = removeSpecial(value.name.last.toLowerCase());
 
     return first.includes(text) || last.includes(text);
   });
@@ -101,4 +101,10 @@ function createElement(element, value) {
   }
 
   return elemento;
+}
+
+function removeSpecial(text) {
+  const map={"â":"a","Â":"A","à":"a","À":"A","á":"a","Á":"A","ã":"a","Ã":"A","ê":"e","Ê":"E","è":"e","È":"E","é":"e","É":"E","î":"i","Î":"I","ì":"i","Ì":"I","í":"i","Í":"I","õ":"o","Õ":"O","ô":"o","Ô":"O","ò":"o","Ò":"O","ó":"o","Ó":"O","ü":"u","Ü":"U","û":"u","Û":"U","ú":"u","Ú":"U","ù":"u","Ù":"U","ç":"c","Ç":"C"};
+
+  return text.replace(/[\W\[\] ]/g,function(a){return map[a]||a});
 }
