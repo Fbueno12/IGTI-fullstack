651ab1a525aa1c1433eec2f425e31ef56371138f
fbueno12 <felipebueno181@gmail.com>
2020-09-01 11:14:52 -0300

    ajustes no app e fechada de pacote

--- modulo-04/desafio/grades-api/app.js
+++ modulo-04/desafio/grades-api/app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:8080',
+    origin: 'http://localhost:3000',
   })
 );
 
--- modulo-04/desafio/grades-api/controllers/gradeController.js
+++ modulo-04/desafio/grades-api/controllers/gradeController.js
@@ -1,18 +1,18 @@
-import { db, GradesModel } from '../models/index.js';
-import { logger } from '../config/logger.js';
-import createGradeService from '../services/createGradeService.js';
-import updateGradeService from '../services/updateGradeService.js';
+import { db, GradesModel } from "../models/index.js";
+import { logger } from "../config/logger.js";
+import createGradeService from "../services/createGradeService.js";
+import updateGradeService from "../services/updateGradeService.js";
 
 const create = async (req, res) => {
   try {
-    res.send({ message: 'Grade inserido com sucesso' });
+    res.send({ message: "Grade inserido com sucesso" });
     const grade = createGradeService.execute(req);
 
     logger.info(`POST /grade - ${JSON.stringify(grade)}`);
   } catch (error) {
     res
       .status(500)
-      .send({ message: error.message || 'Algum erro ocorreu ao salvar' });
+      .send({ message: error.message || "Algum erro ocorreu ao salvar" });
     logger.error(`POST /grade - ${JSON.stringify(error.message)}`);
   }
 };
@@ -22,17 +22,21 @@ const findAll = async (req, res) => {
 
   //condicao para o filtro no findAll
   var condition = name
-    ? { name: { $regex: new RegExp(name), $options: 'i' } }
+    ? { name: { $regex: new RegExp(name), $options: "i" } }
     : {};
 
   try {
     logger.info(`GET /grade`);
-    const grades = await GradesModel.find({});
+    console.log(condition.name);
+    const grades =
+      !condition.name
+        ? await GradesModel.find({})
+        : await GradesModel.find({ name: condition.name.$regex || "" });
     return res.json(grades);
   } catch (error) {
     res
       .status(500)
-      .send({ message: error.message || 'Erro ao listar todos os documentos' });
+      .send({ message: error.message || "Erro ao listar todos os documentos" });
     logger.error(`GET /grade - ${JSON.stringify(error.message)}`);
   }
 };
@@ -45,7 +49,7 @@ const findOne = async (req, res) => {
     const grade = await GradesModel.findById(id);
     return res.json(grade);
   } catch (error) {
-    res.status(500).send({ message: 'Erro ao buscar o Grade id: ' + id });
+    res.status(500).send({ message: "Erro ao buscar o Grade id: " + id });
     logger.error(`GET /grade - ${JSON.stringify(error.message)}`);
   }
 };
@@ -53,7 +57,7 @@ const findOne = async (req, res) => {
 const update = async (req, res) => {
   if (!req.body) {
     return res.status(400).send({
-      message: 'Dados para atualizacao vazio',
+      message: "Dados para atualizacao vazio",
     });
   }
 
@@ -64,7 +68,7 @@ const update = async (req, res) => {
     const grade = await updateGradeService.execute(req);
     return res.json(grade);
   } catch (error) {
-    res.status(500).send({ message: 'Erro ao atualizar a Grade id: ' + id });
+    res.status(500).send({ message: "Erro ao atualizar a Grade id: " + id });
     logger.error(`PUT /grade - ${JSON.stringify(error.message)}`);
   }
 };
@@ -74,12 +78,12 @@ const remove = async (req, res) => {
 
   try {
     logger.info(`DELETE /grade - ${id}`);
-    await GradesModel.remove({_id: id});
-    return res.json({message: "Grade removida com sucesso"});
+    await GradesModel.remove({ _id: id });
+    return res.json({ message: "Grade removida com sucesso" });
   } catch (error) {
     res
       .status(500)
-      .send({ message: 'Nao foi possivel deletar o Grade id: ' + id });
+      .send({ message: "Nao foi possivel deletar o Grade id: " + id });
     logger.error(`DELETE /grade - ${JSON.stringify(error.message)}`);
   }
 };
@@ -88,9 +92,9 @@ const removeAll = async (req, res) => {
   try {
     logger.info(`DELETE /grade`);
     await GradesModel.remove();
-    return res.json({message: "Grades removidas com sucesso"});
+    return res.json({ message: "Grades removidas com sucesso" });
   } catch (error) {
-    res.status(500).send({ message: 'Erro ao excluir todos as Grades' });
+    res.status(500).send({ message: "Erro ao excluir todos as Grades" });
     logger.error(`DELETE /grade - ${JSON.stringify(error.message)}`);
   }
 };
